0ce2ea84c8da824ffa557b601d26d57d3fb419a9
Sergio Perez Gago <sergiopga@ext.inditex.com>
2022-03-22 10:58:19 +0100

    Created test for service to check if the order is not retrieved the correct Exception is Thrown. Test fail as there is no implementation at the moment for this check

--- src/main/java/com/sacks/codeexercise/service/ProductRefundServiceImpl.java
+++ src/main/java/com/sacks/codeexercise/service/ProductRefundServiceImpl.java
@@ -1,11 +1,20 @@
 package com.sacks.codeexercise.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sacks.codeexercise.model.OrderRefundInformation;
+import com.sacks.codeexercise.repository.OrdersRepository;
 
 @Service
 public class ProductRefundServiceImpl implements ProductRefundService {
+
+    private final OrdersRepository ordersRepository;
+
+    @Autowired
+    public ProductRefundServiceImpl(OrdersRepository ordersRepository) {
+        this.ordersRepository = ordersRepository;
+    }
 
     @Override
     public OrderRefundInformation returnProductAndGetRefund(Long orderId, Long productId) {
--- src/test/java/com/sacks/codeexercise/service/ProductRefundServiceImplTest.java
+++ src/test/java/com/sacks/codeexercise/service/ProductRefundServiceImplTest.java
@@ -1,9 +1,45 @@
 package com.sacks.codeexercise.service;
 
+import java.util.Optional;
+
+import com.sacks.codeexercise.error.NotFoundOrderErrorException;
+import com.sacks.codeexercise.model.entities.Order;
+import com.sacks.codeexercise.repository.OrdersRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.lenient;
 
 @ExtendWith(MockitoExtension.class)
 class ProductRefundServiceImplTest {
 
+    @Mock
+    private OrdersRepository ordersRepository;
+
+    private ProductRefundServiceImpl productRefundService;
+
+    @BeforeEach
+    void init(){
+        productRefundService = new ProductRefundServiceImpl(ordersRepository);
+    }
+
+    @Test
+    public void givenAnOrderIdNonExistingToRefundAProductWhenServiceTriesToFindOrderInformationInformationThenOrderNotFoundExceptionIsThrown(){
+
+        Optional<Order> order = Optional.empty();
+        lenient().when(ordersRepository.findOrderByOrderId(100L)).thenReturn(order);
+
+        Exception exception = assertThrows(NotFoundOrderErrorException.class, () -> {
+            productRefundService.returnProductAndGetRefund(100L,1L);
+        });
+
+        String expectedMessage = "Order not found";
+        String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
 }

4bd1645447fa7b49c95a82bd48de32ee33e369e9
Sergio Perez Gago <sergiopga@ext.inditex.com>
2022-03-22 12:54:03 +0100

    Created Test and Implementation to refund a product return

--- src/main/java/com/sacks/codeexercise/service/ProductRefundServiceImpl.java
+++ src/main/java/com/sacks/codeexercise/service/ProductRefundServiceImpl.java
@@ -1,5 +1,6 @@
 package com.sacks.codeexercise.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,29 +10,52 @@ import org.springframework.stereotype.Service;
 import com.sacks.codeexercise.error.NotFoundOrderErrorException;
 import com.sacks.codeexercise.error.ProductNotFoundException;
 import com.sacks.codeexercise.model.OrderRefundInformation;
+import com.sacks.codeexercise.model.ProductInformation;
+import com.sacks.codeexercise.model.ProductStockInformation;
+import com.sacks.codeexercise.model.entities.Customer;
 import com.sacks.codeexercise.model.entities.Order;
 import com.sacks.codeexercise.model.entities.Product;
+import com.sacks.codeexercise.repository.CustomerRepository;
 import com.sacks.codeexercise.repository.OrdersRepository;
 
 @Service
 public class ProductRefundServiceImpl implements ProductRefundService {
 
     private final OrdersRepository ordersRepository;
+    private final CustomerRepository customerRepository;
 
     @Autowired
-    public ProductRefundServiceImpl(OrdersRepository ordersRepository) {
+    public ProductRefundServiceImpl(OrdersRepository ordersRepository,
+        CustomerRepository customerRepository) {
         this.ordersRepository = ordersRepository;
+        this.customerRepository = customerRepository;
     }
 
     @Override
     public OrderRefundInformation returnProductAndGetRefund(Long orderId, Long productId) {
         final Optional<Order> order = ordersRepository.findOrderByOrderId(orderId);
+        OrderRefundInformation orderRefundInformation = new OrderRefundInformation();
+
         if(order.isPresent()){
-            List<Product> productListInOrder = order.get().getProducts();
+            Order orderToRefund = order.get();
+            List<Product> productListInOrder = orderToRefund.getProducts();
             Optional<Product> matchingProduct = productListInOrder.stream().
                 filter(product -> (product.getProductId()) == productId).
                 findFirst();
             if (matchingProduct.isPresent()){
+                Product productToRefund = matchingProduct.get();
+                Customer customerToBeRefunded = customerRepository.findByUsername(order.get().getBuyer().getUsername());
+                Double amountToRefund = productToRefund.getPrice();
+                Double orderAmountAfterRefund = customerToBeRefunded.getCurrentAmountInWallet() + amountToRefund;
+                customerToBeRefunded.setCurrentAmountInWallet(orderAmountAfterRefund);
+                customerToBeRefunded = customerRepository.save(customerToBeRefunded);
+                orderToRefund.setBuyer(customerToBeRefunded);
+                orderToRefund.setAmount(orderAmountAfterRefund);
+                productListInOrder.removeIf(product -> product.getProductId() == productId);
+                orderToRefund.setProducts(productListInOrder);
+                orderToRefund = ordersRepository.save(orderToRefund);
+
+                orderRefundInformation = createOrderRefundedInformation(orderRefundInformation,orderToRefund,productListInOrder);
 
             }else{
                 throw new ProductNotFoundException("Product not found in order. Product can not be refunded");
@@ -39,6 +63,24 @@ public class ProductRefundServiceImpl implements ProductRefundService {
         }else{
             throw new NotFoundOrderErrorException("Order not found");
         }
-        return null;
+        return orderRefundInformation;
+    }
+
+    private OrderRefundInformation createOrderRefundedInformation(OrderRefundInformation orderRefundInformation,Order orderToRefund, List<Product> productListInOrder){
+        orderRefundInformation.setOrderId(orderToRefund.getOrderId());
+        orderRefundInformation.setAmount(orderToRefund.getAmount());
+        orderRefundInformation.setBuyer(orderToRefund.getBuyer().getUsername());
+        orderRefundInformation.setOrderStatus(orderToRefund.getOrderStatus().getStatus());
+        orderRefundInformation.setEstimatedDays(orderToRefund.getEstimatedDays());
+        orderRefundInformation.setAmountInWalletAfterRefund(orderToRefund.getBuyer().getCurrentAmountInWallet());
+
+        List<ProductInformation> productInformationList = new ArrayList<>();
+        productListInOrder.forEach(product -> {
+            ProductInformation productInformation = new ProductInformation(product.getName(),product.getPrice());
+            productInformationList.add(productInformation);
+        });
+        orderRefundInformation.setProducts(productInformationList);
+
+        return orderRefundInformation;
     }
 }
--- src/test/java/com/sacks/codeexercise/service/ProductRefundServiceImplTest.java
+++ src/test/java/com/sacks/codeexercise/service/ProductRefundServiceImplTest.java
@@ -6,10 +6,12 @@ import java.util.Optional;
 
 import com.sacks.codeexercise.error.NotFoundOrderErrorException;
 import com.sacks.codeexercise.error.ProductNotFoundException;
+import com.sacks.codeexercise.model.OrderRefundInformation;
 import com.sacks.codeexercise.model.entities.Customer;
 import com.sacks.codeexercise.model.entities.Order;
 import com.sacks.codeexercise.model.entities.OrderStatus;
 import com.sacks.codeexercise.model.entities.Product;
+import com.sacks.codeexercise.repository.CustomerRepository;
 import com.sacks.codeexercise.repository.OrdersRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,8 +19,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.lenient;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,12 +30,14 @@ class ProductRefundServiceImplTest {
 
     @Mock
     private OrdersRepository ordersRepository;
+    @Mock
+    private CustomerRepository customerRepository;
 
     private ProductRefundServiceImpl productRefundService;
 
     @BeforeEach
     void init(){
-        productRefundService = new ProductRefundServiceImpl(ordersRepository);
+        productRefundService = new ProductRefundServiceImpl(ordersRepository, customerRepository);
     }
 
     @Test
@@ -56,7 +62,7 @@ class ProductRefundServiceImplTest {
         lenient().when(ordersRepository.findOrderByOrderId(1L)).thenReturn(order);
 
         Exception exception = assertThrows(ProductNotFoundException.class, () -> {
-            productRefundService.returnProductAndGetRefund(1L,2L);
+            productRefundService.returnProductAndGetRefund(1L,3L);
         });
 
         String expectedMessage = "Product not found in order. Product can not be refunded";
@@ -64,17 +70,41 @@ class ProductRefundServiceImplTest {
         assertTrue(actualMessage.contains(expectedMessage));
     }
 
+    @Test
+    public void givenAnOrderThatExistTryingToRefundAProductWhenServiceTriesToRefundProductThenOrderIsUpdatedAndCustomerIsRefunded(){
+
+        Optional<Order> order = Optional.of(createOrder());
+        Order orderRefunded = createOrderRefunded();
+        Customer customer = createCustomer();
+        Customer customerRefunded = createCustomerRefunded();
+        lenient().when(ordersRepository.findOrderByOrderId(1L)).thenReturn(order);
+        lenient().when(customerRepository.findByUsername("customer1")).thenReturn(customer);
+        lenient().when(customerRepository.save(any())).thenReturn(customerRefunded);
+        lenient().when(ordersRepository.save(any())).thenReturn(orderRefunded);
+
+        final OrderRefundInformation orderRefundInformation = productRefundService.returnProductAndGetRefund(1L, 1L);
+
+        assertThat(orderRefundInformation.getOrderId()).isEqualTo(orderRefunded.getOrderId());
+        assertThat(orderRefundInformation.getOrderStatus()).isEqualTo(orderRefunded.getOrderStatus().getStatus());
+        assertThat(orderRefundInformation.getAmount()).isEqualTo(orderRefunded.getAmount());
+        assertThat(orderRefundInformation.getBuyer()).isEqualTo(orderRefunded.getBuyer().getUsername());
+        assertThat(orderRefundInformation.getAmountInWalletAfterRefund()).isEqualTo(orderRefunded.getBuyer().getCurrentAmountInWallet());
+        assertThat(orderRefundInformation.getProducts().get(0).getProductName()).isEqualTo(orderRefunded.getProducts().get(0).getName());
+        assertThat(orderRefundInformation.getProducts().get(0).getPrice()).isEqualTo(orderRefunded.getProducts().get(0).getPrice());
+
+    }
+
     private Order createOrder(){
         Order order = new Order();
         List<Product> products = createProducts();
         List<Order> customerOrders = new ArrayList<>();
-        Customer customer = createCustomer();
+        Customer customer = createCustomerRefunded();
         OrderStatus orderStatus = createOrderStatusOrdered();
 
         order.setEstimatedDays(1);
-        order.setAmount(50.0);
+        order.setAmount(150.0);
         order.setProducts(products);
-        order.setOrderId(15015);
+        order.setOrderId(1);
         order.setOrderStatus(orderStatus);
         order.setBuyer(customer);
 
@@ -84,15 +114,41 @@ class ProductRefundServiceImplTest {
         return order;
     }
 
+    private Order createOrderRefunded(){
+        Order order = new Order();
+        List<Product> products = createProductsRefunded();
+        List<Order> customerOrders = new ArrayList<>();
+        Customer customer = createCustomerRefunded();
+        OrderStatus orderStatus = createOrderStatusOrdered();
+
+        order.setEstimatedDays(1);
+        order.setAmount(100.0);
+        order.setProducts(products);
+        order.setOrderId(1);
+        order.setOrderStatus(orderStatus);
+        order.setBuyer(customer);
+
+        customerOrders.add(order);
+        customer.setOrders(customerOrders);
+
+        return order;
+    }
     private List<Product> createProducts(){
-        Product product = new Product();
-        product.setProductId(1);
-        product.setQuantity(10);
-        product.setPrice(50.0);
-        product.setName("Product 1");
+        Product product1 = new Product();
+        product1.setProductId(1);
+        product1.setQuantity(10);
+        product1.setPrice(50.0);
+        product1.setName("Product 1");
+
+        Product product2 = new Product();
+        product2.setProductId(2);
+        product2.setQuantity(10);
+        product2.setPrice(100.0);
+        product2.setName("Product 2");
 
         List<Product> products = new ArrayList<>();
-        products.add(product);
+        products.add(product1);
+        products.add(product2);
 
         return products;
     }
@@ -100,7 +156,17 @@ class ProductRefundServiceImplTest {
     private Customer createCustomer(){
         Customer customer = new Customer();
 
-        customer.setCurrentAmountInWallet(2000.0);
+        customer.setCurrentAmountInWallet(1850.0);
+        customer.setInitialAmountInWallet(2000.0);
+        customer.setUsername("customer1");
+
+        return customer;
+    }
+
+    private Customer createCustomerRefunded(){
+        Customer customer = new Customer();
+
+        customer.setCurrentAmountInWallet(1900.0);
         customer.setInitialAmountInWallet(2000.0);
         customer.setUsername("customer1");
 
@@ -110,5 +176,18 @@ class ProductRefundServiceImplTest {
     private OrderStatus createOrderStatusOrdered(){
         OrderStatus statusPackaged = new OrderStatus(1,"Ordered");
         return statusPackaged;
+    }
+
+    private List<Product> createProductsRefunded() {
+        Product product = new Product();
+        product.setProductId(2);
+        product.setQuantity(10);
+        product.setPrice(100.0);
+        product.setName("Product 2");
+
+        List<Product> products = new ArrayList<>();
+        products.add(product);
+
+        return products;
     }
 }

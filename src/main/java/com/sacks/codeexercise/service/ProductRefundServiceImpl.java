ac5123bd354ac8f4444eacacacc290ddaa9a85b6
Sergio Perez Gago <sergiopga@ext.inditex.com>
2022-03-22 11:01:43 +0100

    Created implementation for failing test

--- src/main/java/com/sacks/codeexercise/service/ProductRefundServiceImpl.java
+++ src/main/java/com/sacks/codeexercise/service/ProductRefundServiceImpl.java
@@ -1,9 +1,13 @@
 package com.sacks.codeexercise.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sacks.codeexercise.error.NotFoundOrderErrorException;
 import com.sacks.codeexercise.model.OrderRefundInformation;
+import com.sacks.codeexercise.model.entities.Order;
 import com.sacks.codeexercise.repository.OrdersRepository;
 
 @Service
@@ -18,6 +22,12 @@ public class ProductRefundServiceImpl implements ProductRefundService {
 
     @Override
     public OrderRefundInformation returnProductAndGetRefund(Long orderId, Long productId) {
+        final Optional<Order> order = ordersRepository.findOrderByOrderId(orderId);
+        if(order.isPresent()){
+
+        }else{
+            throw new NotFoundOrderErrorException("Order not found");
+        }
         return null;
     }
 }
